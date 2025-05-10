b6136fb22cd042d1096a4ed9932d81b2cee6a122
WillFrancois <williamjcfrancois@gmail.com>
2025-05-10 05:59:55 -0400

    Day 3 Part 1 completed

---
+++ Supplies_In_Surplus/part1.cpp
@@ -0,0 +1,36 @@
+#include <fstream>
+#include <iostream>
+
+int main() {
+  std::ifstream input("input.txt");
+  std::string receiver;
+
+  long long acc = 0;
+
+  while (!input.eof()) {
+    try {
+      getline(input, receiver);
+      int split = receiver.find_first_of(" ");
+
+      std::string l = receiver.substr(0, split);
+      std::string r = receiver.substr(split, receiver.size());
+
+      int split_l = l.find_first_of("-");
+      int split_r = r.find_first_of("-");
+
+      std::string l_l = l.substr(0, split_l);
+      std::string l_r = l.substr(split_l + 1, l.size());
+      std::string r_l = r.substr(0, split_r);
+      std::string r_r = r.substr(split_r + 1, r.size());
+
+      acc = acc + (std::stoll(l_r) - std::stoll(l_l)) + 1;
+      acc = acc + (std::stoll(r_r) - std::stoll(r_l)) + 1;
+
+    } catch (std::out_of_range) {
+    }
+  }
+
+  std::cout << acc << std::endl;
+
+  return 0;
+}
