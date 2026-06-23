f202f8f8d7ebadfc978c9d213937d26d010dfdfd
WillFrancois <williamjcfrancois@gmail.com>
2026-06-23 11:00:59 -0400

    Problem 13 Part 1 Completed

---
+++ Laestrygonian_Guards/parts/part1.go
@@ -0,0 +1,37 @@
+package parts
+
+import (
+	"fmt"
+	"Laestrygonian_Guards/types"
+	"maps"
+	"slices"
+)
+
+func dfs (root *types.Node, n_map *map[string]*types.Node, d_map *map[string]int, running_distance int) {
+	r := (*n_map)[root.Name]
+	r.Visited = true
+	val, ok := (*d_map)[r.Name]
+	fmt.Println(r.Name)
+	if !ok || running_distance < val {
+		(*d_map)[r.Name] = running_distance
+	}
+	for i := range r.Connections {
+		if (*n_map)[r.Connections[i]].Visited != true {
+			dfs((*n_map)[r.Connections[i]], n_map, d_map, running_distance + 1)
+		}
+	}
+	r.Visited = false
+}
+
+func Part1 (node_map map[string]*types.Node) int {
+	distance_map := make(map[string]int, len(node_map))
+	root := node_map["STT"]
+	dfs(root, &node_map, &distance_map, 0)
+
+	fmt.Println(distance_map)
+	s := slices.Collect(maps.Values(distance_map))
+	slices.Sort(s)
+	slices.Reverse(s)
+	fmt.Println(s)
+	return s[0] * s[1] * s[2]
+}
